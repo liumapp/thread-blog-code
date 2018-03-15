6549157d897786a595f88749ee4e6dd56efece7c
liumapp <liumapp.com@gmail.com>
2018-03-15 20:27:34 +0800

    rm unnecessary files & rname main class

---
+++ src/main/java/com/liumapp/blog/thread/ThreadBlog.java
@@ -0,0 +1,20 @@
+package com.liumapp.blog.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file Main.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 3/15/18
+ */
+@SpringBootApplication
+public class ThreadBlog {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadBlog.class , args);
+    }
+
+}
