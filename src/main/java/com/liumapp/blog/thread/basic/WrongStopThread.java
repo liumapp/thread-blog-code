4acbb746674d593b046eda6eb0530435e153dcf0
liumapp <liumapp.com@gmail.com>
2018-04-05 11:59:43 +0800

    add wrong stop example

--- src/main/java/com/liumapp/blog/thread/basic/WrongStopThread.java
+++ src/main/java/com/liumapp/blog/thread/basic/WrongStopThread.java
@@ -8,8 +8,19 @@ package com.liumapp.blog.thread.basic;
  * @homepage http://www.liumapp.com
  * @date 4/5/18
  */
-public class WrongStopThread {
+public class WrongStopThread extends Thread {
 
-
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0 ; i < 500000 ; i++) {
+            if (this.isInterrupted()) {
+                System.out.println("under stop status .");
+                break;
+            }
+            System.out.println("i = " + i);
+        }
+        System.out.println("if you view this information , it means your thread stopped wrong ");
+    }
 
 }
---
+++ src/test/java/com/liumapp/blog/thread/basic/StopThreadTest.java
@@ -0,0 +1,18 @@
+package com.liumapp.blog.thread.basic;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author liumapp
+ * @file StopThreadTest.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 4/5/18
+ */
+@RunWith(SpringRunner.class)
+public class StopThreadTest {
+
+
+
+}
