0a4480bfbc65b289a2d6691135aed0ed885361a7
liumapp <liumapp.com@gmail.com>
2018-04-05 11:55:10 +0800

    add wrong stop thread

---
+++ src/main/java/com/liumapp/blog/thread/basic/WrongStopThread.java
@@ -0,0 +1,15 @@
+package com.liumapp.blog.thread.basic;
+
+/**
+ * 错误的线程停止方式
+ * @author liumapp
+ * @file WrongStopThread.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 4/5/18
+ */
+public class WrongStopThread {
+
+
+
+}
