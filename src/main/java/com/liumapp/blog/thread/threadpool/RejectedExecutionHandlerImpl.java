480e67ca4ee88c0b6922de64dbb81cdfb0219ebd
liumapp <liumapp.com@gmail.com>
2018-04-27 14:38:23 +0800

    add rejected execution handler

---
+++ src/main/java/com/liumapp/blog/thread/threadpool/RejectedExecutionHandlerImpl.java
@@ -0,0 +1,21 @@
+package com.liumapp.blog.thread.threadpool;
+
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author liumapp
+ * @file RejectedExecutionHandlerImpl.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 4/27/18
+ */
+public class RejectedExecutionHandlerImpl implements RejectedExecutionHandler {
+
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        System.out.println(r.toString() + " is rejected");
+    }
+
+
+}
--- src/main/java/com/liumapp/blog/thread/threadpool/SimpleThreadPool.java
+++ src/main/java/com/liumapp/blog/thread/threadpool/SimpleThreadPool.java
@@ -1,5 +1,8 @@
 package com.liumapp.blog.thread.threadpool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author liumapp
  * @file SimpleThreadPool.java
@@ -8,4 +11,17 @@ package com.liumapp.blog.thread.threadpool;
  * @date 4/27/18
  */
 public class SimpleThreadPool {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            Runnable worker = new WorkerThread("" + i);
+            executor.execute(worker);
+        }
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+        System.out.println("Finished all threads");
+    }
+
 }
