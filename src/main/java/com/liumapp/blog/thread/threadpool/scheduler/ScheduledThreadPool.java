26c59460ef31e4ef3ed2847cc2471264ddb705dc
liumapp <liumapp.com@gmail.com>
2018-04-27 15:26:00 +0800

    add schedule thread pool

---
+++ src/main/java/com/liumapp/blog/thread/threadpool/scheduler/ScheduledThreadPool.java
@@ -0,0 +1,41 @@
+package com.liumapp.blog.thread.threadpool.scheduler;
+
+import com.liumapp.blog.thread.threadpool.WorkerThread;
+
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author liumapp
+ * @file ScheduledThreadPool.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 4/27/18
+ */
+public class ScheduledThreadPool {
+
+    public static void main(String[] args) throws InterruptedException {
+        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+
+        //schedule to run after sometime
+        System.out.println("Current Time = "+new Date());
+        for(int i=0; i<3; i++){
+            Thread.sleep(1000);
+            WorkerThread worker = new WorkerThread("do heavy processing");
+            scheduledThreadPool.schedule(worker, 10, TimeUnit.SECONDS);
+        }
+
+        //add some delay to let some threads spawn by scheduler
+        Thread.sleep(30000);
+
+        scheduledThreadPool.shutdown();
+        while(!scheduledThreadPool.isTerminated()){
+            //wait for all tasks to finish
+        }
+        System.out.println("Finished all threads");
+    }
+
+
+}
